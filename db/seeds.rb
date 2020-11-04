c8712110e3704d463f3952d172852042f65d59c7
Hirem Pervaz <hirem.p@gmail.com>
2020-11-04 16:14:56 +0400

    models created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+# puts "Cleaning database..."
+# Cocktail.destroy_all
+
+# puts "Creating cocktails..."
+# mojito = { name: "Mojito" }
+# bloody_mary = { name: "Bloody Mary" }
+# sex_on_the_beach = { name: "Sex on the beach" }
+# margarita = { name: "Margarita" }
+# pina_colada = { name: "Pina Colada" }
+
+# [mojito, bloody_mary, sex_on_the_beach, margarita, pina_colada].each do |attributes|
+#   cocktail = Cocktail.create!(attributes)
+#   puts "Created #{cocktail.name}"
+# end
+# puts "Finished!"
